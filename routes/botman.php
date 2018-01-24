41f89e8c387e8ebb21ad16052cf6b339fcdca525
Joe Dixon <hello@joedixon.co.uk>
2018-01-24 22:32:11 +0000

    Initial commit of articles

--- routes/botman.php
+++ routes/botman.php
@@ -1,13 +1,24 @@
 <?php
 
+use App\Services\RssFeed;
 use BotMan\BotMan\BotMan;
 use App\Conversations\Onboarding;
 use App\Middleware\ManagesUsersMiddleware;
+use JoeDixon\BotManDrivers\Extensions\TemplateTemplate;
 
 $botman = resolve('botman');
 $middleware = new ManagesUsersMiddleware;
 $botman->middleware->received($middleware);
 $botman->middleware->matching($middleware);
+
+$botman->hears('ARTICLES', function (BotMan $bot) {
+    $rss = new RssFeed('https://medium.com/feed/@joedixon');
+    $article = $rss->all()->last();
+    $template = TemplateTemplate::create($article->getTitle());
+    $template->addImage($article->getImage());
+    $template->addUrl($article->getImage());
+    $bot->reply($template);
+});
 
 $botman->fallback(function (BotMan $bot) {
     if ($bot->getMessage()->getExtras('is_new_user')) {
