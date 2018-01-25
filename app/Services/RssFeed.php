85b2619406952b83c65b9a9961e2e726ac1a6fa3
Joe Dixon <hello@joedixon.co.uk>
2018-01-25 22:23:39 +0000

    Wrapper around Simplepie RSS parser

--- app/Services/RssFeed.php
+++ app/Services/RssFeed.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use SimplePie;
+use Carbon\Carbon;
 
 class RssFeed
 {
@@ -57,9 +58,19 @@ class RssFeedItem
         return $this->item->get_id();
     }
 
+    public function getUrl()
+    {
+        return $this->item->get_permalink();
+    }
+
     public function getTitle()
     {
         return $this->item->get_title();
+    }
+
+    public function getDescription()
+    {
+        return strip_tags($this->item->get_description());
     }
 
     public function getContent()
@@ -67,8 +78,23 @@ class RssFeedItem
         return $this->item->get_content();
     }
 
+    public function hasImage()
+    {
+        return $this->getImage() === '' ? false : true;
+    }
+
     public function getImage()
     {
-        return str_before(str_after($this->getContent(), 'src="'), '"');
+        return preg_match('/(https?:\/\/.*\.(?:png|jpg))/i', $this->item->get_content(), $matches) === 1 ? $matches[0] : false;
+    }
+
+    public function getTags()
+    {
+        return $this->item->get_item_tags();
+    }
+
+    public function getDate()
+    {
+        return new Carbon($this->item->get_date());
     }
 }
