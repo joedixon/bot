bce3bd2b4814b9e029727bd6077d379385120b8d
Joe Dixon <hello@joedixon.co.uk>
2018-01-29 22:12:09 +0000

    Join mailing list conversation

--- routes/botman.php
+++ routes/botman.php
@@ -2,6 +2,7 @@
 
 use BotMan\BotMan\BotMan;
 use App\Conversations\Onboarding;
+use App\Conversations\JoinMailingList;
 use App\Middleware\ManagesUsersMiddleware;
 
 $botman = resolve('botman');
@@ -10,6 +11,10 @@ $botman->middleware->received($middleware);
 $botman->middleware->matching($middleware);
 
 $botman->hears('articles', 'App\Commands@latestArticles');
+
+$botman->hears('list', function (BotMan $bot) {
+    $bot->startConversation(new JoinMailingList);
+});
 
 $botman->fallback(function (BotMan $bot) {
     if ($bot->getMessage()->getExtras('is_new_user')) {
---
+++ tests/BotMan/JoinMailingListTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\BotMan;
+
+use App\User;
+use Newsletter;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class JoinMailingListTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function a_user_can_signup_to_the_mailing_list()
+    {
+        factory(User::class)->create(['channel_id' => 12345678]);
+
+        Newsletter::shouldReceive('subscribePending')
+            ->once()
+            ->with('test@justtesting.com');
+
+        $this->bot->setUser(['id' => 12345678])
+            ->receives('List')
+            ->assertReply(trans('join_mailing_list.ask_for_email_address'))
+            ->receives('test@justtesting.com')
+            ->assertReply(trans('join_mailing_list.thank_you'));
+    }
+
+    /** @test */
+    public function a_user_cannot_signup_to_the_mailing_list_with_an_invalid_email_address()
+    {
+        factory(User::class)->create(['channel_id' => 12345678]);
+
+        Newsletter::shouldReceive('subscribePending')
+            ->never();
+
+        $this->bot->setUser(['id' => 12345678])
+            ->receives('List')
+            ->assertReply(trans('join_mailing_list.ask_for_email_address'))
+            ->receives('testjusttesting.com')
+            ->assertReply(trans('join_mailing_list.validate_email'));
+    }
+}
