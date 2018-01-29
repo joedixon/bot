ca91c531dfce7e03200b2df1865d0ca9b53cd849
Joe Dixon <hello@joedixon.co.uk>
2018-01-29 09:18:55 +0000

    Update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,8 @@ $factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'channel_id' => $faker->randomNumber(8),
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
     ];
 });
