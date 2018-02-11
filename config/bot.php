0fd10895f3388024de6d253b3ec5836b605ec1f8
Joe Dixon <hello@joedixon.co.uk>
2018-02-11 11:26:45 -0500

    Small talk

--- config/bot.php
+++ config/bot.php
@@ -4,5 +4,7 @@ return [
     'rss_feed_urls' => [
         'https://joedixon.co.uk/archive.rss',
         'https://medium.com/feed/@joedixon',
-    ]
+    ],
+
+    'dialog_flow_token' => env('DIALOG_FLOW_TOKEN'),
 ];
--- routes/botman.php
+++ routes/botman.php
@@ -2,13 +2,15 @@
 
 use BotMan\BotMan\BotMan;
 use App\Conversations\Onboarding;
+use BotMan\BotMan\Middleware\ApiAi;
 use App\Conversations\JoinMailingList;
 use App\Middleware\ManagesUsersMiddleware;
 
 $botman = resolve('botman');
-$middleware = new ManagesUsersMiddleware;
-$botman->middleware->received($middleware);
-$botman->middleware->matching($middleware);
+$usersMiddleware = new ManagesUsersMiddleware;
+$botman->middleware->received($usersMiddleware);
+$botman->middleware->matching($usersMiddleware);
+$botman->middleware->received(ApiAi::create(config('bot.dialog_flow_token')));
 
 $botman->hears('articles', 'App\Commands@latestArticles');
 
@@ -17,7 +19,11 @@ $botman->hears('list', function (BotMan $bot) {
 });
 
 $botman->fallback(function (BotMan $bot) {
+    // if the user is new, fire the onboarding sequence
     if ($bot->getMessage()->getExtras('is_new_user')) {
         return $bot->startConversation(new Onboarding);
     }
+
+    // delegate to DialogFlow to determine response
+    return $bot->reply($bot->getMessage()->getExtras('apiReply'));
 });
